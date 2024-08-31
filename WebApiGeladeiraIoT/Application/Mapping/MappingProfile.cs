3b3cc60865baeb86deb1be72730c6e7a0767a4e0
marina.varela <marina.varela@hotmail.com>
2024-08-30 23:47:55 -0300

    feat: crud. aguardando testes

---
+++ WebApiGeladeiraIoT/Application/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using ApiGeladeira.DTOs;
+using ApiGeladeira.Models;
+using AutoMapper;
+
+namespace Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateGeladeiraDTO, ItemGeladeira>();
+            CreateMap<UpdateGeladeiraDTO, ItemGeladeira>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+        }
+    }
+}
