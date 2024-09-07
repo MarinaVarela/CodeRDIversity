e268f9ea974830dcc864ce2a30a8ab2440bc196f
marina.varela <marina.varela@hotmail.com>
2024-09-07 00:40:20 -0300

    Exercício V - Arquitetura

--- WebApiGeladeiraIoT/ApiGeladeira/Program.cs
+++ WebApiGeladeiraIoT/ApiGeladeira/Program.cs
@@ -1,23 +1,30 @@
-using ApiGeladeira.Repository;
 using Application.Mapping;
-using Application.Services;
+using Infrastructure.Config;
 using Infrastructure.Repositories;
+using IoC;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<GeladeiraRepository>();
-builder.Services.AddScoped<GeladeiraService>();
+DependencyInjection.RegisterServices(builder.Services);
 
 builder.Services.AddDbContext<GeladeiraContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+builder.Services.AddSwaggerGen(options =>
+{
+    SwaggerConfig.ConfigureSwagger(options);
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
---
+++ WebApiGeladeiraIoT/Domain/Interfaces/IGeladeiraService.cs
@@ -0,0 +1,15 @@
+﻿using ApiGeladeira.Models;
+
+namespace Domain.Interfaces
+{
+    public interface IGeladeiraService
+    {
+        Task<ItemGeladeira?> AdicionarItemAsync(ItemGeladeira item);
+        Task<ItemGeladeira?> AtualizarItem(ItemGeladeira item);
+        Task<ItemGeladeira?> ObterItemPorId(int id);
+        Task<ItemGeladeira?> ObterItemPorNome(string nome);
+        Task<List<ItemGeladeira>> ObterItens();
+        Task<ItemGeladeira?> RemoverItem(int id);
+        Task<int> RemoverTodosItens();
+    }
+}
